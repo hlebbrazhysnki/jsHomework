b2829997de6018edbd2d16a956b93ae66f2d390e
Hleb Brazhynski <hleb200217@gmail.com>
2021-10-11 01:20:08 -0400

    jsHomework

---
+++ video-3/index.js
@@ -0,0 +1,15 @@
+var myVariableGleb;
+
+myVariableGleb = 10;
+
+var myVar2 = "hello";
+
+myVar2
+myVar2 = "hello again";
+
+myVar2 = 5 
+
+var customerAge = 32;
+
+var myVar3 = 40; 
+
---
+++ video-4/index.js
@@ -0,0 +1,51 @@
+var myVar = 5
+
+myVar
+5
+
+5+5
+10 
+
+myVar + 10
+15
+
+myVar
+5
+
+myVar = myVar + 10
+15
+myVar
+15
+
+10-5
+5
+
+myVar - 3 
+12
+
+myVar = myVar - 5 
+10
+myVar
+10
+
+10 * 10
+100
+20/10
+2
+myVar = myVar / 2 
+5
+myVar
+5
+
+5+5
+10
+
+100 + "hello"
+"100hello"
+
+"hello" + "david"
+"hellodavid"
+
+"hello" + " david"
+"hello david"
+
